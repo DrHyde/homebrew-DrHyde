e500affe7e0d562f0c3d5e3f38cfa98a27df7d52
David Cantrell <david@cantrell.org.uk>
2022-02-25 21:02:29 +0000

    declare dependency on rust

--- Formula/toml-merge.rb
+++ Formula/toml-merge.rb
@@ -3,6 +3,8 @@ class TomlMerge < Formula
   homepage "https://github.com/mrnerdhair/toml-merge"
   head "https://github.com/mrnerdhair/toml-merge.git", branch: "master"
 
+  depends_on "rust" => :build
+
   def install
 
     system "cargo", "build", "--release"

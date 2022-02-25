56ddb56ebb324c6854d17a83c7946b15271ac219
David Cantrell <david@cantrell.org.uk>
2022-02-25 20:47:24 +0000

    Add formula for toml-merge

---
+++ Formula/toml-merge.rb
@@ -0,0 +1,13 @@
+class TomlMerge < Formula
+  desc "Merge TOML files"
+  homepage "https://github.com/mrnerdhair/toml-merge"
+  head "https://github.com/mrnerdhair/toml-merge.git", branch: "master"
+
+  def install
+
+    system "cargo build --release"
+    bin.install "target/release/toml-merge"
+
+  end
+
+end

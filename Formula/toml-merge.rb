87fdcca1df5a1527b7b751bc755ee4835ac212d9
David Cantrell <david@cantrell.org.uk>
2022-02-25 22:14:43 +0000

    use this specific revision which I've code-reviewed, not whatever is current HEAD

--- Formula/toml-merge.rb
+++ Formula/toml-merge.rb
@@ -1,7 +1,7 @@
 class TomlMerge < Formula
   desc "Merge TOML files"
   homepage "https://github.com/mrnerdhair/toml-merge"
-  head "https://github.com/mrnerdhair/toml-merge.git", branch: "master"
+  head "https://github.com/mrnerdhair/toml-merge.git", revision: "c44eee7c7fa52e98b34c20c88d19979adcafcf1b"
 
   depends_on "rust" => :build
 
